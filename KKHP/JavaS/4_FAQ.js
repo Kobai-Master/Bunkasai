961e4d1652ce9fe87501df7a2e372419211aea1c
daonan523 <kameestima@gmail.com>
2024-09-10 21:57:42 +0900

    Add files via upload

--- KKHP/JavaS/4_FAQ.js
+++ KKHP/JavaS/4_FAQ.js
@@ -4,10 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     faqs.forEach(function(faq) {
         faq.addEventListener('click', function() {
             var answer = this.nextElementSibling;
+            var symbol = this.querySelector('.toggle-symbol');
+            
+            // 回答の表示・非表示を切り替える
             if (answer.style.display === 'block') {
                 answer.style.display = 'none';
+                symbol.textContent = '＋';
             } else {
                 answer.style.display = 'block';
+                symbol.textContent = '−';
             }
         });
     });
